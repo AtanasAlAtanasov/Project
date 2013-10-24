001a71f63fcb6e39bec01a20ff4c4fe7c2bda9d0
Atanas Atanasov <Nasko@Atanass-MacBook-Pro.local>
2013-10-24 18:46:03 +0300

    Initial Commit

---
+++ Sofia_Trasport/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sofia_Trasport
+//
+//  Created by Atanas Atanasov on 10/24/13.
+//  Copyright (c) 2013 Atanas Atanasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sofia_TrasportTests/Sofia_TrasportTests.m
@@ -0,0 +1,34 @@
+//
+//  Sofia_TrasportTests.m
+//  Sofia_TrasportTests
+//
+//  Created by Atanas Atanasov on 10/24/13.
+//  Copyright (c) 2013 Atanas Atanasov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sofia_TrasportTests : XCTestCase
+
+@end
+
+@implementation Sofia_TrasportTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
