04b94df647561f0c4c05c6832c2a48c565d89747
Atanas Atanasov <Nasko@Atanass-MacBook-Pro.local>
2013-10-25 00:59:10 +0300

    Parse Sofia public transport information

--- Sofia_Trasport/ViewController.m
+++ Sofia_Trasport/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "TFHpple.h"
+#import "Tutorial.h"
+#import "Contributor.h"
 
 @interface ViewController ()
 
@@ -19,6 +22,45 @@
     [super viewDidLoad];
 	
     mapView.showsUserLocation = YES;
+    [self loadStops];
+}
+
+-(void)loadStops {
+    
+    NSURL *stopsUrl = [NSURL URLWithString:@"https://code.google.com/p/sofia-public-transport-navigator/source/browse/res/raw/coordinates.xml?name=Version+1.2+-+fixed+map+key"];
+    NSLog(@"1 %@",stopsUrl);
+    NSData *stopsHtmlData = [NSData dataWithContentsOfURL:stopsUrl];
+   
+    // 2
+    TFHpple *stopsParser = [TFHpple hppleWithHTMLData:stopsHtmlData];
+    
+
+    // 3
+    NSString *stopsXpathQueryString = @"//*[@id='src_table_0']/tr/td";
+    NSLog(@"2 %@",stopsXpathQueryString);
+    NSArray *stopsNodes = [stopsParser searchWithXPathQuery:stopsXpathQueryString];
+    //NSLog(@"5 %@",stopsNodes);
+
+    
+    // 4
+    //NSMutableArray *newStops = [[NSMutableArray alloc] initWithCapacity:0];
+   
+    for (TFHppleElement *element in stopsNodes) {
+        // 5
+        Tutorial *transportStop = [[Tutorial alloc] init];
+        //[newStops addObject:transportStop];
+        
+        // 6
+        
+        transportStop.title = [[element firstChild] content];
+        NSLog(@"3 %@",transportStop.title);
+        
+        // 7
+        //transportStop.url = [element objectForKey:@"atv"];
+        //NSLog(@"4 %@",transportStop.url);
+        
+
+    }
 }
 
 - (void)didReceiveMemoryWarning
