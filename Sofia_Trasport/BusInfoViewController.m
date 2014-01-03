5e4597803dfd74a2693ac7b0bb00f84d9d7bc29e
Atanas Atanasov <Nasko@Atanass-MacBook-Pro.local>
2014-01-03 21:59:30 +0200

    so close but still so far

--- Sofia_Trasport/BusInfoViewController.m
+++ Sofia_Trasport/BusInfoViewController.m
@@ -12,23 +12,15 @@
 
 @interface BusInfoViewController ()
 
+@property (nonatomic, strong) NSString *cookieMonster;
+
 @end
 
 @implementation BusInfoViewController
 @synthesize lableName,nameOfStop,lableCode,codeOfStop,stringForParse,parseString,codeWebView,textCodeView,url,stringCodeCheck;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
-   
     [super viewDidLoad];
     
     lableName.text = nameOfStop;
@@ -37,24 +29,27 @@
     NSString *last = [NSString stringWithFormat:@"&o=1&go=1"];
     url = [NSString stringWithFormat:@"%@%@%@",first,codeOfStop,last];
     [self getDataFrom:url];
-
 }
 
-- (NSString *) getDataFrom:(NSString *)myUrl{
+- (NSString *) getDataFrom:(NSString *)myUrl
+{
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     
     [request setHTTPMethod:@"GET"];
     [request setURL:[NSURL URLWithString:myUrl]];
     
     NSError *error = [[NSError alloc] init];
-    NSHTTPURLResponse *responseCode = nil;
+    NSHTTPURLResponse *response = nil;
     
-    NSData *oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
+    NSData *oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
     
-    if([responseCode statusCode] != 200){
-        NSLog(@"Error getting %@, HTTP status code %i", myUrl, [responseCode statusCode]);
+    if ([response statusCode] != 200) {
+        NSLog(@"Error getting %@, HTTP status code %i", myUrl, [response statusCode]);
         return nil;
     }
+    
+    //NSLog(@"All Response Header Fields : %@", response.allHeaderFields);
+    self.cookieMonster = response.allHeaderFields[@"Set-Cookie"];
     
     stringForParse = [[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding];
     parseString = [stringForParse componentsSeparatedByString:@"src=""\""];
@@ -68,8 +63,8 @@
     stringCodeCheck = [stringForParse  substringFromIndex:9];
     first = [NSString stringWithFormat:@"\""];
     
-    NSLog(@"url %@",myUrl);
-    NSLog(@"pic Code %@",stringCodeCheck);
+    //NSLog(@"url %@",myUrl);
+    //NSLog(@"pic Code %@",stringCodeCheck);
     
     NSURL *urlAdress = [NSURL URLWithString:myUrl];
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:urlAdress];
@@ -80,22 +75,26 @@
 
 - (IBAction)checkButton:(id)sender {
     [self postDataFrom:url];
-    NSLog(@"code %@",textCodeView.text);
+    //NSLog(@"code %@",textCodeView.text);
 }
 
-- (NSString *) postDataFrom:(NSString *)myUrl{
+- (NSString *) postDataFrom:(NSString *)myUrl
+{
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     
     [request setHTTPMethod:@"POST"];
+    //NSLog(@"url: %@",myUrl);
     [request setURL:[NSURL URLWithString:myUrl]];
-    
+    assert(self.cookieMonster.length);
+    [request setValue:self.cookieMonster forHTTPHeaderField:@"Set-Cookie"];
+    //NSLog(@"All Response Header Fields : %@", request.allHTTPHeaderFields);
     NSError *error = [[NSError alloc] init];
-    NSHTTPURLResponse *responseCode = nil;
+    NSHTTPURLResponse *response = nil;
     
-    NSData *oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
+    NSData *oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
     
-    if([responseCode statusCode] != 200){
-        NSLog(@"Error getting %@, HTTP status code %i", myUrl, [responseCode statusCode]);
+    if([response statusCode] != 200){
+        NSLog(@"Error getting %@, HTTP status code %i", myUrl, [response statusCode]);
         return nil;
     }
     
@@ -105,17 +104,42 @@
     
     stringForParse=[parseString[2] substringWithRange:NSMakeRange(9, 32)];
 
-    NSLog(@"pic Code Check %@",stringForParse);
+    //NSLog(@"pic Code Check %@",stringForParse);
     
-    NSLog(@"%@",[[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding]);
+    NSString *first = [NSString stringWithFormat:@"value=\""];
+    NSString *sec =[NSString stringWithFormat:@"\"/>"];
+    NSString *all  = [NSString stringWithFormat:@"%@%@%@",first,stringForParse,sec];
+    first = [NSString stringWithFormat:@"value=\""];
+    sec =[NSString stringWithFormat:@"\"/>"];
+    NSString *allSecond  = [NSString stringWithFormat:@"%@%@%@",first,stringCodeCheck,sec];
+    
+    NSString *stringForBody = [[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding];
+    stringForBody = [stringForBody stringByReplacingOccurrencesOfString:all withString:allSecond];
     
     
-    [request addValue:textCodeView.text forHTTPHeaderField:@"Content-Type"];
+    first = [NSString stringWithFormat:@"<input class=\"inp\" id=\"sc\" name=\"sc\" type=\"text\" maxlength=\"10\" value=\""];
+    sec = [NSString stringWithFormat:@"\"/>"];
+    allSecond = [NSString stringWithFormat:@"%@%@%@",first,textCodeView.text,sec];
     
-    [request setValue:stringForParse forHTTPHeaderField:stringCodeCheck];
-    NSLog(@"%@",[[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding]);
-
-    return [[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding];
+    stringForBody = [stringForBody stringByReplacingOccurrencesOfString:@"<input class=\"inp\" id=\"sc\" name=\"sc\" type=\"text\" maxlength=\"10\"/>" withString:allSecond];
+    
+    
+    first = [NSString stringWithFormat:@"<input type=\"text\"  value=\""];
+    sec = [NSString stringWithFormat:@"\"  name=\"q\" maxlength=\"40\" class=\"inp\"/>"];
+     allSecond = [NSString stringWithFormat:@"%@%@%@",first,lableCode.text,sec];
+    stringForBody = [stringForBody stringByReplacingOccurrencesOfString:@"<input type=\"text\"  value=\"\"  name=\"q\" maxlength=\"40\" class=\"inp\"/>"  withString:allSecond];
+    
+    
+    NSMutableData *data = [NSMutableData data];
+    [data appendData:[stringForBody dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    [request setHTTPBody:data];
+    
+    //NSLog(@"All Response Header Fields : %@", response.allHeaderFields);
+    //oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    //NSLog(@"response: %@",[[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding]);
+    //NSLog(@"return: %@",[[NSString alloc] initWithData:request.HTTPBody encoding:NSUTF8StringEncoding]);
+    return [[NSString alloc] initWithData:request.HTTPBody encoding:NSUTF8StringEncoding];
     
 }
 
